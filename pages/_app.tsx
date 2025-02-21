e4642c1377490fbd7b724ce855a0b5c9577cec43
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2025-02-21 10:52:39 +0700

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+        <link rel="icon" href="/Logo.png" />
+        <title>Link HMSE</title>
+      </Head>
       <main>
         <Component {...pageProps} />
       </main>
